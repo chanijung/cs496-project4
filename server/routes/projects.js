e045b4a70c69bcf9845c2cbf2ff9c29645dba69e
chanijung <1016chani@gmail.com>
2021-01-26 08:48:20 +0000

    secret key undefiend resolved

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -9,7 +9,8 @@ router.use(require('cookie-parser')());
 
 const jwt = require('jsonwebtoken');
 var dotenv = require('dotenv');
-dotenv.config({path:'../../.env'});
+// dotenv.config({path:'../../.env'});
+dotenv.config();
 const SECRET_KEY = process.env.SECRET_KEY;
 
 const Project = require('../models/project');
